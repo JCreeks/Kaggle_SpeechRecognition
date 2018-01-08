def83fd4c5d696176cb45b1883c2d9f7b47fda42
Ubuntu <ubuntu@ip-172-31-3-128.us-east-2.compute.internal>
2018-01-08 07:36:20 +0000

    1/7/18 update

--- model/lightWeight_CNN.py
+++ model/lightWeight_CNN.py
@@ -1,9 +1,3 @@
-# coding: utf-8
-
-
-# In[1]:
-
-
 import os
 import sys
 import numpy as np
@@ -25,7 +19,15 @@ from keras.models import load_model
 module_path = os.path.abspath(os.path.join('..'))
 sys.path.append(module_path)
 
+# from keras import backend as K
+# import tensorflow as tf
 
+# config = tf.ConfigProto(intra_op_parallelism_threads=args.jobs, 
+#                         inter_op_parallelism_threads=args.jobs, 
+#                         allow_soft_placement=True, 
+#                         device_count = {'CPU': args.jobs})
+# session = tf.Session(config=config)
+# K.set_session(session)
 
 from conf.configure import Configure
 from utils import data_util
@@ -35,34 +37,38 @@ relabel = relabel()
 new_sample_rate = sampleRate()#8000
 chopNum = 1000
 L = 16000
+hyper_pwr = 0.3
+filtersList = [16,32,64] #default [8,16,32]
+seed = 2018
+np.random.seed(seed)
+epoch = 10 #default 3
+batch_size = 20
 legal_labels = 'yes no up down left right on off stop go silence unknown'.split()
+label_index = ['down', 'go', 'left', 'no', 'off', 'on', 'right', 'silence', 'stop', 'unknown', 'up', 'yes']
 
-# def label_transform(labels, relabel=True, get_dummies=True):
-#     nlabels = []
-#     if relabel:
-#         for label in labels:
-#             if label == '_background_noise_':
-#                 nlabels.append('silence')
-#             elif label not in legal_labels:
-#                 nlabels.append('unknown')
-#             else:
-#                 nlabels.append(label)
-#     else:
-#         nlabels = labels
-#     if get_dummies:
-#         return(pd.get_dummies(pd.Series(nlabels)))
-#     return labels
+# Function to compute class weights
+def comp_cls_wts(y, pwr = 0.2):
+    '''
+    Used to compute class weights
+    '''
+    labelCount = y.sum(axis=0)
+    dic = {}
+    for i in range(y.shape[1]):
+        print(labelCount[i])
+        dic[i] = labelCount[i]**pwr/np.sum(labelCount)**pwr
+    return dic
 
 x_train, y_train = data_util.load_train()
 
+cls_wts = comp_cls_wts(y_train, pwr = hyper_pwr)
+print(cls_wts)
+
 input_shape = (x_train.shape[1], x_train.shape[2], x_train.shape[3])
 nclass = y_train.shape[1]#12
-seed = 2017
+
 inp = Input(shape=input_shape)
 norm_inp = BatchNormalization()(inp)
-filtersList = [16,32,64] #default [8,16,32]
-epoch = 10 #default 3
-batch_size = 20
+
 img_1 = Convolution2D(filtersList[0], kernel_size=2, activation=activations.relu)(norm_inp)
 img_1 = Convolution2D(filtersList[0], kernel_size=2, activation=activations.relu)(img_1)
 img_1 = MaxPooling2D(pool_size=(2, 2))(img_1)
@@ -87,7 +93,8 @@ model.compile(optimizer=opt, loss=losses.binary_crossentropy)
 model.summary()
 
 x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.1, random_state=seed)
-model.fit(x_train, y_train, batch_size=batch_size, validation_data=(x_valid, y_valid), epochs=epoch, shuffle=True, verbose=2)
+model.fit(x_train, y_train, batch_size=batch_size, validation_data=(x_valid, y_valid), epochs=epoch, 
+          class_weight = cls_wts, shuffle=True, verbose=2)
 
 modelName = 'sampleRate'+str(new_sample_rate)+'_nclass'+str(nclass)+'_seed'+str(seed)+'_batchSize'+str(batch_size)            +'_epoch'+str(epoch)+'_ConvDouble'+'.model'
 model.save(os.path.join(Configure.model_path, modelName))
@@ -97,7 +104,6 @@ gc.collect()
 
 x_test, test_fname = data_util.load_test()
 
-label_index = ['down', 'go', 'left', 'no', 'off', 'on', 'right', 'silence', 'stop', 'unknown', 'up', 'yes']
 print("start predicting...")
 predList = []
 predicts = model.predict(x_test)
